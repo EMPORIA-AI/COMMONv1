bbec77b56b9839b9ca0b395d75b834bb6dadb04d
scott91e1 <sm@intermine.com>
2021-12-14 11:26:57 +1100

    🐱‍👤🐱‍👤🐱‍👤

--- common/schema.py
+++ common/schema.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8
-# SPDX-License-Identifier: CC0-1.0
+# SPDX-License-Identifier: CC0-1.0 & Additional T&Cs
 # Copyright (c) 12021 - 12021 HE, Emporia.AI Pte Ltd
 # See LICENSE.md for Additional Terms and Conditions
-
-import pendulum
 
 from typing import Any, IO, Optional, List, Dict
 from pydantic.dataclasses import dataclass
 from decimal import *
+
+import pendulum
 
 from .base import *
 
--- common/space.py
+++ common/space.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8
-# SPDX-License-Identifier: CC0-1.0
+# SPDX-License-Identifier: CC0-1.0 & Additional T&Cs
 # Copyright (c) 12021 - 12021 HE, Emporia.AI Pte Ltd
 # See LICENSE.md for Additional Terms and Conditions
 
